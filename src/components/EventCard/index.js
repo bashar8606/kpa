7c483cb0454441ddb38dd2e95773009e49867e21
bashar8606 <ahammed.bashar9@gmail.com>
2023-09-10 01:54:26 -0700

    latest

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -7,11 +7,13 @@ const EventCard = (props) => {
         <div className={style.card}>
             <div className={style.card_img}>
                 <div className="ratio">
-                <Image src="/images/cover.webp" fill alt="shape" />
+                    <Image src="/images/cover.webp" fill alt="shape" />
                 </div>
             </div>
             <div className={style.card_body}>
-
+                <p>Sep 22nd 2023</p>
+                <h2 className="h4 fw-bold text-white ff-secondary mb-3 mb-lg-4">KPA Malappuram District Conference</h2>
+                <a href="#">Enroll Now</a>
             </div>
         </div>
     );
